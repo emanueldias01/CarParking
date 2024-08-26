73ce862a601ae86737dcd65b0ed1661a28d12425
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-08-26 12:32:40 -0300

    criacao da validacao de vaga

---
+++ CarParking/src/main/kotlin/br/com/emanueldias/CarParking/validation/VagaValidation.kt
@@ -0,0 +1,29 @@
+package br.com.emanueldias.CarParking.validation
+
+import br.com.emanueldias.CarParking.mapper.VeiculoFromVeiculoResponseMapper
+import br.com.emanueldias.CarParking.repository.VeiculoRepository
+import org.springframework.stereotype.Service
+
+@Service
+class VagaValidation(
+    private val veiculoRepository: VeiculoRepository,
+    private val veiculoFromVeiculoResponseMapper: VeiculoFromVeiculoResponseMapper
+) {
+
+    fun validaVaga(numeroVagaEscolhida : Int) : Boolean{
+        val listVagas = veiculoRepository.findAll().map { v -> veiculoFromVeiculoResponseMapper.map(v) }.toList()
+        var cont = 0
+
+        for(veiculo in listVagas){
+            if(veiculo.numeroEstacionamento == numeroVagaEscolhida)
+                cont++
+            break
+        }
+
+        return if(cont != 0){
+            false
+        }else{
+            true
+        }
+    }
+}
